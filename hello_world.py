a088ecfe99cc8e5a0788133995bde03aa6f9737c
Vinod K <vinodk@jecc.ac.in>
2018-07-12 19:45:20 +0530

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# Printing Hello World!
+print("Hello World!")
+
+# Another method of printing Hello World!
+print("Hello", "World!")
+
+# Printing Hello World! with help of a variable
+myvariable = "Hello World!"
+print(myvariable)
+
+# Printing Hello World! by concatenating string Hello with variable which contains World! - Method 1
+myvariable = "World!"
+print("Hello "+ myvariable)
+
+# Printing Hello World! by concatenating string Hello with variable which contains World! - Method 2
+myvariable = "World!"
+print("Hello", myvariable)
