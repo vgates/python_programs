6ffe9768221106dd3c122c7da5a6ab4095c0c7a8
Vinod K <vinodk@jecc.ac.in>
2018-09-02 13:47:15 +0530

    power

---
+++ p016_power_using_math_module.py
@@ -0,0 +1,19 @@
+# Find x raised to the power y using math module in Python
+
+# math is an inbuilt python module
+# Python module is a normal python file which can store 
+# function, variable, classes, constants etc.
+
+
+import math # imports math module
+
+# get the user input x and y
+# int function is used to convert the user input to integer
+x = int( input("Enter x: ") )
+y = int( input("Enter y: ") )
+
+# Calculate the power using the function pow() defined in the 
+# math module we imported.
+result = math.pow(x, y)
+
+print("{x} raised to the power {y} =".format(x=x,y=y), result)
